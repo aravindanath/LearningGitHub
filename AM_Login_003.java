99c3fc5860bfaad5693f1590931f9452692d0bcb
aravindanath <aravindanath86@gmail.com>
2018-11-24 09:39:11 +0530

    First commit by adding java file

---
+++ AM_Login_003.java
@@ -0,0 +1,28 @@
+package testScripts;
+
+import java.io.IOException;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+ 
+import pages.HomePage;
+import pages.LoginPage;
+import pages.ProductPage;
+
+public class AM_Login_003 extends BaseDev {
+
+	@Test
+	public void AM_Login_003() throws InterruptedException, IOException {
+
+		LoginPage lp = new LoginPage(driver);
+		lp.login("AM_LG01");
+
+		HomePage hp = new HomePage(driver);
+		hp.searchProduct("AM_HP01");
+
+		ProductPage pp = new ProductPage(driver);
+		pp.productDiscription();
+	//	Assert.fail("This case is fail");
+	}
+}
